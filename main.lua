280b9c3645377ab3a45fd651795de71ea74d22cb
Chang Chen <cc2757@columbia.edu>
2015-04-29 03:16:43 -0400

    start

--- main.lua
+++ main.lua
@@ -11,22 +11,8 @@ print(opt)
 
 torch.setdefaulttensortype('torch.FloatTensor')
 
-cutorch.setDevice(opt.GPU) -- by default, use GPU 1
-torch.manualSeed(opt.manualSeed)
-
-print('Saving everything to: ' .. opt.save)
-os.execute('mkdir -p ' .. opt.save)
-
 paths.dofile('data.lua')
-paths.dofile('model.lua')
-paths.dofile('train.lua')
-paths.dofile('test.lua')
 paths.dofile('util.lua')
 
-epoch = opt.epochNumber
 
-for i=1,opt.nEpochs do
-   train()
-   test()
-   epoch = epoch + 1
-end
+
